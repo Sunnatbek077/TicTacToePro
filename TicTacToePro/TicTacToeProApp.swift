0800ba55c0b7c515db744256decfeeb117c42e0b
Sunnatbek077 <sunnatbekabdunabiyev@icloud.com>
2025-09-20 22:12:33 +0500

    Initial Commit

---
+++ TicTacToePro/TicTacToeProApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeProApp.swift
+//  TicTacToePro
+//
+//  Created by Sunnatbek on 20/09/25.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
