078c84630075167e8c295dfdbf1c144d333e52f3
Sunnatbek077 <sunnatbekabdunabiyev@icloud.com>
2025-11-29 14:36:13 +0500

    Create a new target for WatchOS

---
+++ TicTacToePro (watchOS APP) Watch App/TicTacToePro__watchOS_APP_App.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToePro__watchOS_APP_App.swift
+//  TicTacToePro (watchOS APP) Watch App
+//
+//  Created by Sunnatbek on 28/11/25.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToePro__watchOS_APP__Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
